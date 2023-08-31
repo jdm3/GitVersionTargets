801aa287af7aff20e9a339014d80d286703409ec
Jefferson Montgomery <jdm3@users.noreply.github.com>
2023-08-31 07:46:46 -0700

    Initial commit

---
+++ sample/sample.cpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2023 Jefferson Montgomery
+// SPDX-License-Identifier: MIT
+#include <stdio.h>
+
+int main(int, char**)
+{
+    printf("GVT_BRANCH = %s\n", GVT_BRANCH);
+    printf("GVT_COMMIT = %s\n", GVT_COMMIT);
+    return 0;
+}
